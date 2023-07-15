7b1a02f4400ee9c4d3168ffb837a266eb62e09a1
yourk <yourk123@hotmail.com>
2023-07-15 13:09:57 -0600

    Listo

--- Hackaton.py
+++ Hackaton.py
@@ -1,16 +1,13 @@
-import openai  
+import openai
 import os
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 import whisper
 import tempfile
-from PIL import Image
-import requests
-from io import BytesIO
 import pyttsx3
-from IPython.display import Audio
 
-openai.api_key  = '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'
+# Set up your OpenAI API credentials
+openai.api_key = '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'
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
@@ -20,6 +17,7 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
         temperature=0,
     )
     return response.choices[0].message["content"]
+
 def text_to_speech(text):
     engine = pyttsx3.init()
     with tempfile.NamedTemporaryFile(delete=True) as temp_audio:
@@ -28,40 +26,84 @@ def text_to_speech(text):
         engine.runAndWait()
         audio_data = open(temp_audio_path, "rb").read()
         return audio_data
-def main(): 
-    st.title("Oral Essay")
-    audio_bytes = audio_recorder()
-    if audio_bytes:
-        with open("audio.wav", "wb") as audio_file:
-            audio_file.write(audio_bytes)
-        audio_path = os.path.join(os.getcwd(), "audio.wav")  
-        # Cargar el audio desde el archivo grabado previamente
-        
-    else:
-        audio_path = None
-    if audio_path is not None:
-            audio = whisper.load_audio(audio_path)
-    else:
-            audio = None
 
-    result = None
-    # Cargar el modelo Whisper
+def translate_text(text, target_language):
+    # Translate text to target language using OpenAI API
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Translate the following {target_language}:\n\n{text}",
+        max_tokens=100,
+        temperature=0.0,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=None,
+        n=1,
+    )
+    translation = response.choices[0].text.strip()
+    return translation
+
+def detect_language(text):
+    # Detect language of the text using OpenAI API
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"What is the language of the following text: \"{text}\"?",
+        max_tokens=50,
+        temperature=0.0,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=None,
+        n=1
+    )
+    language = response.choices[0].text.strip()
+    return language
+
+def customer_support_chat(foreigner_input):
+    # Detect foreign language
+    foreign_language = detect_language(foreigner_input)
+
+    # Translate local support's input to foreigner's language
+    foreigner_message = translate_text(foreigner_input, foreign_language)
+
+    # Prepare the conversation history to be displayed for each user
+    foreigner_display = foreigner_message
+
+    return foreigner_display
+
+st.title("Tourism guide")
+audio_bytes = audio_recorder()
+if audio_bytes:
+    with open("audio.wav", "wb") as audio_file:
+        audio_file.write(audio_bytes)
+    audio_path = os.path.join(os.getcwd(), "audio.wav")
+else:
+    audio_path = None
+
+result = None
+if audio_path is not None:
+    audio = whisper.load_audio(audio_path)
     model = whisper.load_model("base")
-    if audio is not None:
-        result = model.transcribe(audio)
-        st.write(result["text"])
-    else:
-        st.write("No se ha proporcionado ningún audio.")
+    result = model.transcribe(audio)
+else:
+    st.write("No se ha proporcionado ningún audio.")
 
-    if result is not None:
-        prompt = f"Respondeme este texto: {result['text']}"
-        response = get_completion(prompt)
-        st.write(response)
+if result is not None:
+    foreigner_input = result["text"]
+    foreign_language = detect_language(foreigner_input)
 
-        audio_data = text_to_speech(response)
-        st.audio(audio_data, format="audio/wav")
-    else:
-        st.write("No se ha proporcionado ningún texto de entrada.")
- 
-if __name__ == "__main__":
-    main()
+    response = get_completion(foreigner_input)
+
+    # Translate local support's input to foreigner's language
+    foreigner_message = translate_text(response, foreign_language)
+
+    # Prepare the conversation history to be displayed for each user
+    foreigner_display = foreigner_message
+    st.write(foreigner_display)
+
+    audio_data = text_to_speech(foreigner_display)
+    st.audio(audio_data, format="audio/wav")
+else:
+    st.write("No se ha proporcionado ningún texto de entrada.")
+
+foreigner_input = st.text_input("Texto del usuario extranjero")
